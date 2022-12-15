506832ccdea850ab91b084d1922f1c17abdff567
Chinchinadapavani <Chinchinadapavani@Pavanivm>
2022-12-15 18:57:33 +0000

    Add project files.

---
+++ MVC HotelManagement/Repository/ServiceRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace MVC_HotelManagement.Repository
+{
+    public class ServiceRepository
+    {
+        HttpClient client;
+        public ServiceRepository()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(ConfigurationManager.AppSettings["apiBaseURL"].ToString());
+        }
+        public HttpResponseMessage GetResponse(string url)
+        {
+            return client.GetAsync(url).Result;
+        }
+        public HttpResponseMessage PostResponse(string url, object content)
+        {
+            return client.PostAsJsonAsync(url, content).Result;
+        }
+        public HttpResponseMessage DeleteResponse(string url, int Id)
+        {
+            return client.DeleteAsync(url + Id.ToString()).Result;
+        }
+    }
+}
---
+++ WebApi HotelManagement/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApi_HotelManagement.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
